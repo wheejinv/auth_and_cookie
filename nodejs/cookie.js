b8309ab369b63532eef90c2f0701b7ff976c164b
wheejinv <wheejinv@naver.com>
2022-07-24 17:11:10 +0900

    쿠키 설정, 클라에서부터 온 쿠키 까볼려면 req.headers.cookie

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,11 +1,24 @@
 import express from 'express'
+
 const app = express();
 const port = 3003;
 
+
+app.use( (req, res, next) => {
+  res.cookie('hi', 'hello', {
+    expires: new Date(Date.now() + 500000)
+  })
+  next();
+});
+
+
 app.get('/', (req, res) => {
-  res.send('cookie!');
+  // cookie 파서 안쓰면 이런식으로 옴. req.headers.cookie
+  console.log(`req.headers.cookie: ${req.headers.cookie}`);
+
+  res.send(`cookie!`);
 })
 
 app.listen(port, () => {
-  console.log('Server started on port ');
+  console.log(`Server started on port: ${port}`);
 })
