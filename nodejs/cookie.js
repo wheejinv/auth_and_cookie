07c01224402c2ea904bb502ca16bc578f8ecb7e0
wheejinv <wheejinv@naver.com>
2022-07-24 17:43:50 +0900

    cookie-parser 도입 / express 공식문서

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 
 const app = express();
 const port = 3003;
 
-
+app.use(cookieParser());
 app.use( (req, res, next) => {
   res.cookie('hi', 'hello', {
     expires: new Date(Date.now() + 500000)
@@ -13,8 +14,7 @@ app.use( (req, res, next) => {
 
 
 app.get('/', (req, res) => {
-  // cookie 파서 안쓰면 이런식으로 옴. req.headers.cookie
-  console.log(`req.headers.cookie: ${req.headers.cookie}`);
+  console.log(JSON.stringify(req.cookies));
 
   res.send(`cookie!`);
 })
