acbb7965b3d3c334a3708c9ad6755cecf66636ed
wheejinv <wheejinv@naver.com>
2022-07-25 00:31:40 +0900

    로그인 성공 시 쿠키 구움

--- nuxt/backend/app.js
+++ nuxt/backend/app.js
@@ -3,8 +3,11 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const port = process.env.PORT;
+const cookieParser = require("cookie-parser");
 
 app.use(cors({ origin: true }));
+
+app.use(cookieParser());
 
 //// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,6 +25,12 @@ app.post("/login", (req, res) => {
   let result = false;
   if (id === "test" && password === "test") {
     result = true;
+
+    res.cookie("id", id, {
+      maxAge: 1000 * 3600,
+    });
+    res.cookie("password", password);
+    res.cookie("nick", "HeeJin Ha");
   }
 
   res.send({ result });
