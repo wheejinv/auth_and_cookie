46d8af54ecfa5b2fc7e05e72d0e7645b5b6a37d5
wheejinv <wheejinv@naver.com>
2022-07-25 01:01:56 +0900

    코어스 설정

--- nuxt/backend/app.js
+++ nuxt/backend/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const port = process.env.PORT;
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-app.use(cors({ origin: true }));
-
+// https://stackoverflow.com/questions/57009371/access-to-xmlhttprequest-at-from-origin-localhost3000-has-been-blocked
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
 //// parse application/x-www-form-urlencoded
